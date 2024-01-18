46d39cde6e1a69f120029559f920e43489ec8c08
TriQue06 <baristanlik@gmail.com>
2024-01-18 22:43:37 +0300

    Initial commit

---
+++ src/main/java/net/trique/ZekininCevherleriDataGenerator.java
@@ -0,0 +1,11 @@
+package net.trique;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ZekininCevherleriDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
